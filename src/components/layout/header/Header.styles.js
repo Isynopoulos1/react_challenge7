becb7bfc2e6898258b8a311e4783efe218627dbc
Isela <isela.v.alarcon@gmail.com>
2021-12-12 21:59:34 -0600

    creating the container

--- src/components/layout/header/Header.styles.js
+++ src/components/layout/header/Header.styles.js
@@ -3,4 +3,7 @@ import styled from "styled-components";
 export const HeaderContainer = styled.div`
   display: flex;
   font-size: 18px;
+  width: 100%;
+  height: 50px;
+  background-color: #ff6a13;
 `;
