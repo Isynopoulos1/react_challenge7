74f08f0de39d1570a980b5f5eac324a7bbc4597b
Isela <isela.v.alarcon@gmail.com>
2021-12-13 19:42:26 -0600

    styloing the main nav

--- src/components/layout/main/Main.styles.js
+++ src/components/layout/main/Main.styles.js
@@ -2,4 +2,36 @@ import styled from "styled-components";
 
 export const MainContainer = styled.div`
   display: flex;
+  width: 100%;
+
+  align-items: center;
+  justify-content: space-between;
+`;
+export const MainImg = styled.img`
+  display: flex;
+  width: 20%;
+`;
+export const NavMain = styled.div`
+  display: flex;
+`;
+export const Photobooks = styled.div`
+  display: flex;
+`;
+export const Calendars = styled.div`
+  display: flex;
+`;
+export const Cards = styled.div`
+  display: flex;
+`;
+export const Prints = styled.div`
+  display: flex;
+`;
+export const WallArt = styled.div`
+  display: flex;
+`;
+export const Products = styled.div`
+  display: flex;
+`;
+export const Blog = styled.div`
+  display: flex;
 `;
