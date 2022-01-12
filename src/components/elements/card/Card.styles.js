05ee99735ca6e1d381c827f2cdfc56a80bb2d5f0
Isela <isela.v.alarcon@gmail.com>
2022-01-11 20:11:39 -0600

    setting the map

--- src/components/elements/card/Card.styles.js
+++ src/components/elements/card/Card.styles.js
@@ -2,6 +2,11 @@ import styled from "styled-components";
 
 export const CardContainer = styled.div`
   display: flex;
+  flex-direction: column;
+  font-family: helvetica;
+  color: #555;
+  align-items: center;
+  font-size: 15px;
   width: 270px;
   height: 338px;
   border: 1px solid #555;
@@ -14,7 +19,10 @@ export const CardImage = styled.div`
 `;
 export const Products = styled.div`
   display: flex;
+  font-weight: lighter;
+  margin-top: 20px;
 `;
 export const Price = styled.div`
   display: flex;
+  margin-top: 10px;
 `;
---
+++ src/components/elements/data/cards.js
@@ -0,0 +1,26 @@
+export const list1 = [
+  {
+    image:
+      "https://ik.imagekit.io/ppayaz/react_challenge6/Captura_de_Pantalla_2021-12-04_a_la_s__16.41.42_jscgVmhg3.png?updatedAt=1638657857789",
+    product: "right HUB 宁波戴宅",
+    price: "Varios propietarios"
+  },
+  {
+    image:
+      "https://ik.imagekit.io/ppayaz/react_challenge6/Captura_de_Pantalla_2021-12-04_a_la_s__16.41.42_jscgVmhg3.png?updatedAt=1638657857789",
+    product: "right HUB 宁波戴宅",
+    price: "Varios propietarios"
+  },
+  {
+    image:
+      "https://ik.imagekit.io/ppayaz/react_challenge6/Captura_de_Pantalla_2021-12-04_a_la_s__16.41.42_jscgVmhg3.png?updatedAt=1638657857789",
+    product: "right HUB 宁波戴宅",
+    price: "Varios propietarios"
+  },
+  {
+    image:
+      "https://ik.imagekit.io/ppayaz/react_challenge6/Captura_de_Pantalla_2021-12-04_a_la_s__16.41.42_jscgVmhg3.png?updatedAt=1638657857789",
+    product: "right HUB 宁波戴宅",
+    price: "Varios propietarios"
+  }
+];
